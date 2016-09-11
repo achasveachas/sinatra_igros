28963ca661b86d37ca8e9cd277c2ef7f3a26e4fd
Yechiel Kalmenson <chilikkal@gmail.com>
2016-09-11 16:52:22 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 require './config/environment'
+require 'rack-flash'
+
 
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, 'igros'
     set :public_folder, 'public'
     set :views, 'app/views'
+    use Rack::Flash
   end
 
   get "/" do
@@ -13,7 +18,21 @@ class ApplicationController < Sinatra::Base
 
   post "/" do
     @igros = Igros.new(params[:x3], params[:x5],params[:x7])
-    erb :'index.html'
+
+    if !integer?(@igros.x3) || !integer?(@igros.x5) || !integer?(@igros.x7)
+      flash[:message] = "The inputs have to be whole numbers."
+      erb :'index.html'
+    elsif @igros.x3.to_i >= 3 || @igros.x5.to_i >= 5 || @igros.x7.to_i >= 7
+      flash[:message] = "It seems like your math was off, please try again.<br>See below for an example"
+      erb :'index.html'
+    else
+      erb :'index.html'
+    end
   end
 
+  helpers do
+    def integer?(string)
+      string.to_i.to_s == string
+    end
+  end
 end
--- app/models/igros.rb
+++ app/models/igros.rb
@@ -2,13 +2,13 @@ class Igros
   attr_accessor :x3, :x5, :x7
 
   def initialize(three, five, seven)
-    @x3 = three.to_i
-    @x5 = five.to_i
-    @x7 = seven.to_i
+    @x3 = three
+    @x5 = five
+    @x7 = seven
   end
 
   def result
-    number = (@x3 * 70) + (@x5 * 21) + (@x7 * 15)
+    number = (@x3.to_i * 70) + (@x5.to_i * 21) + (@x7.to_i * 15)
     until number <= 105
       number -= 105
     end
